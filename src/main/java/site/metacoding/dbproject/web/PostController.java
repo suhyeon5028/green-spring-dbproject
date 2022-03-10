0e67e172a32362b185868fbd15d6b1cc278dbc1b
suhyeon <suhyeon5028@naver.com>
2022-03-10 12:46:17 +0900

    14. Lazy와 Eager 전략

--- src/main/java/site/metacoding/dbproject/web/PostController.java
+++ src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -55,6 +55,7 @@ public class PostController {
         if (postOp.isPresent()) {
             Post postEntity = postOp.get();
             model.addAttribute("post", postEntity);
+            System.out.println("================================");
             return "post/detail";
         } else {
             return "error/page1";
